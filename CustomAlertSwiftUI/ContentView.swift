b79db96acd7f8594a2199080f2a6fb7be97abf84
Dennis <dnsljnv411@gmail.com>
2023-03-07 18:29:30 +0700

    Create ZStack with buttons

--- CustomAlertSwiftUI/ContentView.swift
+++ CustomAlertSwiftUI/ContentView.swift
@@ -8,11 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAlertShown = true
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button {
+                isAlertShown.toggle()
+            } label: {
+                Text("Stow alert")
+            }
+        }
+        .modifier(AlertViewModifier(isAlertPresenter: $isAlertShown))
     }
 }
+
+
+struct AlertViewModifier: ViewModifier {
+    @Binding var isAlertPresenter: Bool
+//    @Binding var confirmAction: () -> Void
+//    @Binding var dismissAction: () -> Void
+
+
+    func body(content: Content) -> some View {
+        if isAlertPresenter {
+            return AnyView(content)
+        } else {
+            return AnyView(
+            ZStack(alignment: .center) {
+                AlertView(
+                    confirmAction: .constant({}),
+                    dismissAction: .constant({})
+                )
+            }
+            )
+        }
+    }
+}
+
+struct AlertView: View {
+    @Binding var confirmAction: () -> Void
+    @Binding var dismissAction: () -> Void
+
+    var body: some View {
+        createStackOfButtons()
+    }
+
+    private func createStackOfButtons() -> some View {
+        HStack(alignment: .center, spacing: 10) {
+            createConfirmButton()
+            createDismissButton()
+        }
+    }
+
+    private func createConfirmButton() -> some View {
+        Button {
+            confirmAction()
+        } label: {
+            Text("Oтправить")
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+        }
+    }
+
+    private func createDismissButton() -> some View {
+        Button {
+            dismissAction()
+        } label: {
+            Text("Oтменить")
+                .foregroundColor(.gray)
+                .padding()
+                .background(Color.white)
+                .border(.black)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
