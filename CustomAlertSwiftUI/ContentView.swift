a93c323d29b1ca33120082f21002acb00af9f378
Dennis <dnsljnv411@gmail.com>
2023-03-07 17:33:54 +0700

    Initial Commit

---
+++ CustomAlertSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomAlertSwiftUI
+//
+//  Created by Dennis Programmer on 7/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomAlertSwiftUI/CustomAlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertSwiftUIApp.swift
+//  CustomAlertSwiftUI
+//
+//  Created by Dennis Programmer on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
